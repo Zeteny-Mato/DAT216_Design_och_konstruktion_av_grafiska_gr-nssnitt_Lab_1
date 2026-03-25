914a04f1f08d67709fed8688f08846139b265cac
Zéti <zeti.mato@gmail.com>
2026-03-25 15:07:54 +0100

    Update main_view.dart

--- lab1/lib/pages/main_view.dart
+++ lab1/lib/pages/main_view.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:lab1/widgets/task_list.dart';
 
 class MainView extends StatelessWidget {
   const MainView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('DAT216/TIG091'),
-            Text('Lab1 Todos'),
-          ],
-        ),
+    Widget build(BuildContext context) {
+      return Scaffold(
+      appBar: AppBar(
+      title: const Text('Things todo'),
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
+      body: TaskList(),
     );
   }
 }
